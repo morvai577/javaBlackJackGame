affafb04474b08bf92f35dc17c4652e535f48296
Vaibhav Mor <mor.vaibhav577@gmail.com>
2018-01-15 19:42:38 +1300

    Set up deck class methods and finish work on main class

--- src/javaBlackJackGame/Blackjack.java
+++ src/javaBlackJackGame/Blackjack.java
@@ -1,5 +1,7 @@
 package javaBlackJackGame;
 
+import java.util.Scanner;
+
 public class Blackjack {
 
 	public static void main(String[] args) {
@@ -9,7 +11,16 @@ public class Blackjack {
         
         // Create playing deck
         Deck playingDeck = new Deck();
-        playingDeck.createFullDeck();
+        playingDeck.createFullDeck(); // Create a deck of cards
+        playingDeck.shuffle(); // Shuffle Deck
+        
+        // Create a deck for the player
+        Deck playerDeck = new Deck(); // Starts with an empty deck
+        Deck dealerDeck = new Deck();
+        
+        double playerMoney = 100.00;
+        
+        Scanner userInput = new Scanner(System.in);
 
         System.out.println(playingDeck);
 		
--- src/javaBlackJackGame/Deck.java
+++ src/javaBlackJackGame/Deck.java
@@ -4,6 +4,7 @@
 package javaBlackJackGame;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 
 /**
@@ -30,15 +31,56 @@ public class Deck {
         }
     }
 
+    // Shuffle the deck
+    public void shuffle() {
+    		ArrayList<Card> tmpDeck = new ArrayList<Card>();
+    		Random random = new Random(); // Generate random number
+    		int randomCardIndex = 0;
+    		int originalSize = this.cards.size();
+    		
+    		for(int i = 0; i < originalSize; i++) {
+    			// Generate random index
+    			randomCardIndex = random.nextInt((this.cards.size()-1 - 0) + 1) + 0;
+    			tmpDeck.add(this.cards.get(randomCardIndex));
+    			
+    			// Remove from original deck
+    			this.cards.remove(randomCardIndex);
+    		}
+    		
+    		this.cards = tmpDeck;
+    }
+    
     public String toString() {
         String cardListOutput = "";
         int i = 0;
         for (Card aCard : this.cards) {
-            cardListOutput += "\n" + i + "-" + aCard.toString();
+            cardListOutput += "\n " + aCard.toString();
             i++;
         }
 
         return cardListOutput;
     }
+    
+    // Remove the card from the deck
+    public void removeCard(int i) {
+    		this.cards.remove(i);
+    }
+    
+    // Get a card from the deck
+    public Card getCard(int i) {
+    		return this.cards.get(i);
+    }
+    
+    // Add a card to the deck
+    public void addCard(Card addCard) {
+    		this.cards.add(addCard);
+    }
+    
+    // Draw from the deck
+    public void draw(Deck comingFrom) {
+    		this.cards.add(comingFrom.getCard(0));
+    		comingFrom.removeCard(0);
+    }
+    
 
 }
