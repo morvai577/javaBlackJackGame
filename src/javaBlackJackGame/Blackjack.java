9fadf343c565a0dd6d089d14a39096b1e9f95d59
Vaibhav Mor <mor.vaibhav577@gmail.com>
2018-01-18 22:00:44 +1300

    Add game loop and cardsValue method

--- src/javaBlackJackGame/Blackjack.java
+++ src/javaBlackJackGame/Blackjack.java
@@ -23,6 +23,38 @@ public class Blackjack {
         Scanner userInput = new Scanner(System.in);
 
         System.out.println(playingDeck);
+        
+        // Game Loop
+        while (playerMoney > 0) {
+        	
+        		// Continue playing
+        		System.out.println("You have $" + playerMoney + ", how much would you like to bet ?");
+        		double playerBet = userInput.nextDouble();
+        		if (playerBet > playerMoney) {
+        			System.out.println("You cannot bet more than you have. Please try again.");
+        			break;
+        		}
+        		
+        		// Start Dealing
+        		// Player gets two cards
+        		playerDeck.draw(playingDeck);
+        		playerDeck.draw(playingDeck);
+
+        		// Dealer gets two cards
+        		dealerDeck.draw(playingDeck);
+        		dealerDeck.draw(playingDeck);
+        		
+        		while(true) {
+        			System.out.println("Your hand:");
+        			System.out.println(playerDeck.toString());
+        		}
+        		
+        		
+        		
+
+        }
+        
+        System.out.println("Game over! You are out of money. :(");
 		
 	}
 
